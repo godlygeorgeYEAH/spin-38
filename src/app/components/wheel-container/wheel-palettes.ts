a6753c9bce093cd4959b577755d1fa210ce51a2e
godlygeorgeYEAH <godlygeorgegagr@gmail.com>
2026-06-03 18:07:53 -0400

    inversion de los colores ext int de la paleta selva

--- src/app/components/wheel-container/wheel-palettes.ts
+++ src/app/components/wheel-container/wheel-palettes.ts
@@ -32,12 +32,12 @@ export const WHEEL_PALETTES = {
 
   Selva: {
     outerWheelColors: [
-      { stops: [{ offset: '60%', color: '#103EA8' }, { offset: '100%', color: '#061E60' }] },
-      { stops: [{ offset: '60%', color: '#2C7CE0' }, { offset: '100%', color: '#1455B0' }] },
-    ],
-    innerWheelColors: [
       { stops: [{ offset: '60%', color: '#0A5C38' }, { offset: '100%', color: '#042E18' }] },
       { stops: [{ offset: '60%', color: '#1EC468' }, { offset: '100%', color: '#0E8A40' }] },
+    ],
+    innerWheelColors: [
+      { stops: [{ offset: '60%', color: '#103EA8' }, { offset: '100%', color: '#061E60' }] },
+      { stops: [{ offset: '60%', color: '#2C7CE0' }, { offset: '100%', color: '#1455B0' }] },
     ],
   },
 
